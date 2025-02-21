8364de2489bac8501097e0eb517b5acd46b2c40a
m1031 <ro22062b@osaka-pct.ac.jp>
2025-02-21 13:20:09 +0900

    OTP設定とワンタイムキーセットアップのビューを追加し、データベース設定を更新

--- config/database.php
+++ config/database.php
@@ -84,17 +84,18 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'ep-round-dust-a4i1m68z-pooler.us-east-1.aws.neon.tech'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'verceldb'),
+            'username' => env('DB_USERNAME', 'default'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'schema' => 'public',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
